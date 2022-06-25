604173acf0861adccb0ca7d127feec74600c14bf
Oversabirob <105428510+Oversabirob@users.noreply.github.com>
2022-06-25 07:24:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let Name = 'Name: Robert Onwukwe';
+let Height = 'Height: 6.0ft';
+let country = 'country: Nigeria';
+
+console.log(Name);
+console.log(Height);
+console.log(country);
+
+alert(Name + '\n' + Height + '\n' + country);
+
+document.write(Name + '<br>' + Height + '<br>' + country);
+
